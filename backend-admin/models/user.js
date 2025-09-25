a1600fefcb7e064988e846b23280cef4bb6fc57c
Woodley tk <woodleyklavensky@gmail.com>
2025-09-25 16:04:18 -0400

    tables are created

---
+++ backend-admin/models/user.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const bcrypt = require("bcrypt");
+
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Log, {
+        foreignKey: "userId",
+      });
+    }
+  }
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "moderator", "user"),
+        allowNull: false,
+        defaultValue: "user",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeSave: async (user) => {
+          user.password = await bcrypt.hash(user.password, 10);
+        },
+      },
+    }
+  );
+  return User;
+};
